ad45b3f00bab3fee3f60da4b0ec0f417db33c9e8
ACT-STUDENT <ACT-STUDENT@LAB2-PC32>
2024-11-22 11:16:27 +0800

    projects

---
+++ Data/ProjectTask.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BuildMasterPro.Data
+{
+    public class ProjectTask
+    {
+        [Key]
+        public int TaskId { get; set; }
+        [Required]
+        public int ProjectId { get; set; }
+        [Required]
+        public string TaskName { get; set; } = string.Empty;
+        [Required]
+        public string TaskDescription { get; set; } = string.Empty;
+
+        //AssignedTo property later
+        [DataType(DataType.DateTime)]
+        public DateTime StartDate { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime DueDate { get; set; }
+        [AllowNull]
+        public string Status { get; set; } = "Not Started";
+        [AllowNull]
+        public string Priority { get; set; }
+
+        [ForeignKey("ProjectId")]
+        public Project Project { get; set; } = default!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-using BuildMasterPro.Components;
+﻿using BuildMasterPro.Components;
 using BuildMasterPro.Components.Account;
 using BuildMasterPro.Data;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Syncfusion.Blazor;
 using Syncfusion.Blazor.Core;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,8 +28,10 @@ builder.Services.AddAuthentication(options =>
     .AddIdentityCookies();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
@@ -35,7 +40,11 @@ builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.Requ
     .AddDefaultTokenProviders();
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+builder.Services.AddSyncfusionBlazor();
 Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NDaF5cWWtCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWH5ccHRcRGFYUkx3VkE=");
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSyncfusionBlazor();
 
 var app = builder.Build();
 
@@ -49,6 +58,7 @@ else
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
