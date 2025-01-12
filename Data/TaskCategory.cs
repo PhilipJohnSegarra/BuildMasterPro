8e043588cde3ae39af77f8b91edc9220c98cbaba
philip <philipjohn.segarra@gmail.com>
2025-01-12 18:38:48 +0800

    taskedit

---
+++ Data/TaskCategory.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BuildMasterPro.Data
+{
+    public class TaskCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string CategoryName { get; set; } = "";
+        [AllowNull]
+        public string Description { get; set; } = "";
+    }
+}
