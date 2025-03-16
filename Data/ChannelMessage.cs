bf3bab5e0a0f2be5f290164e52c2ac0e8bccb129
Queen Philip Mae <philipjohn.segarra@gmail.com>
2025-03-16 20:58:12 +0800

    New Channel

---
+++ Data/ChannelMessage.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace BuildMasterPro.Data
+{
+    public class ChannelMessage
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string? ChannelMessageId { get; set; }
+        [BsonElement("channel_message")]
+        public string? Message { get; set; }
+        [BsonElement("image_array")]
+        public MongoImageArray? ImageArray { get; set; }
+        [BsonElement("sender_id")]
+        public string? SenderID { get; set; }
+        [BsonElement("sender_name")]
+        public string? SenderName { get; set; } = "";
+        [BsonElement("timestamp")]
+        public DateTime? Timestamp { get; set; } = DateTime.Now;
+    }
+}
