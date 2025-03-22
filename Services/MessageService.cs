4d3d74e73b03a7d0269e2ac241fbbc1bae484d1a
Queen Philip Mae <philipjohn.segarra@gmail.com>
2025-03-22 15:37:31 +0800

    message sync

--- Services/MessageService.cs
+++ Services/MessageService.cs
@@ -1,25 +1,25 @@
 ﻿using BuildMasterPro.Data;
-using Microsoft.EntityFrameworkCore;
-using MongoDB.Bson;
 using MongoDB.Driver;
+using System.Collections.Concurrent;
 
 namespace BuildMasterPro.Services
 {
     public class MessageService
     {
-        MongoService _MongoService;
-        public List<Message> Messages { get; set; } = new();
+        private readonly IMongoCollection<Message> _messageCollection;
+        private readonly ConcurrentBag<Action<Message>> _subscribers = new();
+
         public MessageService(MongoService mongoService)
         {
-            _MongoService = mongoService;
+            _messageCollection = mongoService.GetCollection<Message>("Messages");
+            StartListeningForChanges();
         }
 
         public async Task<List<Message>> GetLatestMessagesForUser(ApplicationUser user)
         {
-            var messageCollection = _MongoService.GetCollection<Message>("Messages");
 
             // Aggregation pipeline using the fluent syntax
-            var messages = await messageCollection.Aggregate()
+            var messages = await _messageCollection.Aggregate()
                 // Match messages where the user is either the sender or the receiver
                 .Match(Builders<Message>.Filter.Or(
                     Builders<Message>.Filter.Eq(m => m.SenderID, user.Id),
@@ -42,11 +42,10 @@ namespace BuildMasterPro.Services
                 .ToListAsync();
 
             return messages.OrderByDescending(m => m.Timestamp).ToList();
-            
         }
-        public async Task<List<Message>> GetAllMessagesForUsers(ApplicationUser user, ApplicationUser? user2)
+
+        public async Task<List<Message>> GetAllMessagesForUsers(ApplicationUser user, ApplicationUser user2)
         {
-            var messageCollection = _MongoService.GetCollection<Message>("Messages");
             var matchStage = Builders<Message>.Filter.Or(
                         Builders<Message>.Filter.And(
                             Builders<Message>.Filter.Eq(m => m.SenderID, user.Id),
@@ -58,12 +57,42 @@ namespace BuildMasterPro.Services
                             )
                     );
             // Aggregation pipeline using the fluent syntax
-            var messages = await messageCollection.Aggregate()
+            var messages = await _messageCollection.Aggregate()
                 // Match messages where the user is either the sender or the receiver
                 .Match(matchStage)
                 .ToListAsync();
 
             return messages.OrderBy(m => m.Timestamp).ToList(); ;
+        }
+
+        public async Task SendMessageAsync(Message message)
+        {
+            await _messageCollection.InsertOneAsync(message);
+        }
+
+        public void SubscribeToMessages(Action<Message> callback)
+        {
+            _subscribers.Add(callback);
+        }
+
+        private void StartListeningForChanges()
+        {
+            var pipeline = new EmptyPipelineDefinition<ChangeStreamDocument<Message>>()
+                .Match("{ operationType: 'insert' }");
+
+            var cursor = _messageCollection.Watch(pipeline);
+
+            Task.Run(async () =>
+            {
+                await cursor.ForEachAsync(change =>
+                {
+                    var newMessage = change.FullDocument;
+                    foreach (var subscriber in _subscribers)
+                    {
+                        subscriber.Invoke(newMessage);
+                    }
+                });
+            });
         }
     }
 }
