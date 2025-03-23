0dfd82246a037d0e2ec6d15e60c434ad33c3b3db
Queen Philip Mae <philipjohn.segarra@gmail.com>
2025-03-23 18:44:38 +0800

    fix tasks not adding and updating

--- Data/TaskUser.cs
+++ Data/TaskUser.cs
@@ -6,6 +6,7 @@ namespace BuildMasterPro.Data
     public class TaskUser
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         [Required]
